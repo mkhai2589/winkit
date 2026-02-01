96d580bf5116a04a5f229891dd3b85feed7cba4f
Minh Khai <57771429+mkhai2589@users.noreply.github.com>
2026-02-01 23:15:08 +0700

    Update Security.ps1

--- core/Security.ps1
+++ core/Security.ps1
@@ -1,9 +1,8 @@
 function Test-WKAdmin {
     $id = [Security.Principal.WindowsIdentity]::GetCurrent()
     $p  = New-Object Security.Principal.WindowsPrincipal($id)
-
+    
     if (-not $p.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-        Write-Host "WinKit must be run as Administrator." -ForegroundColor Red
-        exit 1
+        throw "Administrator privileges required. Please run as admin."
     }
 }
