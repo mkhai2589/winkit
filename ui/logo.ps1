cf97e2e2a2f99c1e987a8bba441182825e6b2f0d
Minh Khai <57771429+mkhai2589@users.noreply.github.com>
2026-02-02 15:29:18 +0700

    Update logo.ps1

--- ui/logo.ps1
+++ ui/logo.ps1
@@ -1,15 +1,40 @@
 function Show-Logo {
+    $width = 76  # Fixed width for header
     $padding = $global:WK_PADDING
     
-    Write-Host "$padding------------------------------------------" -ForegroundColor Cyan
-    Write-Host "$padding              W I N K I T                 " -ForegroundColor White
-    Write-Host "$padding" -ForegroundColor Cyan
-    Write-Host "$padding      __        ___      _  ___ _ _" -ForegroundColor Cyan
-    Write-Host "$padding      \ \      / (_)_ __| |/ (_) | |" -ForegroundColor Cyan
-    Write-Host "$padding       \ \ /\ / /| | '__| ' /| | | |" -ForegroundColor Cyan
-    Write-Host "$padding        \ V  V / | | |  | . \| | | |" -ForegroundColor Cyan
-    Write-Host "$padding         \_/\_/  |_|_|  |_|\_\_|_|_|" -ForegroundColor Cyan
-    Write-Host "$padding" -ForegroundColor Cyan
-    Write-Host "$padding        Windows Optimization Toolkit" -ForegroundColor Gray
-    Write-Host "$padding------------------------------------------" -ForegroundColor Cyan
+    # Calculate centering
+    $title = "W I N K I T"
+    $subtitle = "Windows Optimization Toolkit"
+    $author = "Author: Minh Khai | 0333 090 930"
+    
+    # Top border
+    $borderTop = "╔" + ("═" * ($width - 2)) + "╗"
+    $borderBottom = "╚" + ("═" * ($width - 2)) + "╝"
+    $emptyLine = "║" + (" " * ($width - 2)) + "║"
+    
+    # Title with padding
+    $titlePadding = [math]::Max(0, ($width - 2 - $title.Length)) / 2
+    $leftTitlePad = [math]::Floor($titlePadding)
+    $rightTitlePad = [math]::Ceiling($titlePadding)
+    $titleLine = "║" + (" " * $leftTitlePad) + $title + (" " * $rightTitlePad) + "║"
+    
+    # Subtitle with padding
+    $subPadding = [math]::Max(0, ($width - 2 - $subtitle.Length)) / 2
+    $leftSubPad = [math]::Floor($subPadding)
+    $rightSubPad = [math]::Ceiling($subPadding)
+    $subtitleLine = "║" + (" " * $leftSubPad) + $subtitle + (" " * $rightSubPad) + "║"
+    
+    # Author with padding
+    $authPadding = [math]::Max(0, ($width - 2 - $author.Length)) / 2
+    $leftAuthPad = [math]::Floor($authPadding)
+    $rightAuthPad = [math]::Ceiling($authPadding)
+    $authorLine = "║" + (" " * $leftAuthPad) + $author + (" " * $rightAuthPad) + "║"
+    
+    Write-Host "$padding$borderTop" -ForegroundColor Cyan
+    Write-Host "$padding$titleLine" -ForegroundColor White
+    Write-Host "$padding$subtitleLine" -ForegroundColor Gray
+    Write-Host "$padding$emptyLine" -ForegroundColor Cyan
+    Write-Host "$padding$authorLine" -ForegroundColor Gray
+    Write-Host "$padding$borderBottom" -ForegroundColor Cyan
+    Write-Host ""
 }
