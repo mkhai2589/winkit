817bd5e5bf62ff3a500bc26da11f5646f3aa5087
Minh Khai <57771429+mkhai2589@users.noreply.github.com>
2026-02-02 23:41:26 +0700

    Update Security.ps1

--- core/Security.ps1
+++ core/Security.ps1
@@ -1,14 +1,5 @@
-function Test-WKAdmin {
-    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
-    
-    if (-not $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
-        Write-Host "ERROR: Administrator privileges required." -ForegroundColor Red
-        Write-Host "Please run PowerShell as Administrator and try again." -ForegroundColor Yellow
-        exit 1
-    }
-}
-
 function Test-AdminSilent {
-    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
-    return $currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
+    $id = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $p  = New-Object Security.Principal.WindowsPrincipal($id)
+    $p.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 }
